02df591eead537981ead6f002cb0c13c4086f9b9
Anne L'Hôte <anne.lhote@gmail.com>
2017-12-05 08:19:21 +0100

    Clean code, replace double quote by signle quote

--- index_fr.php
+++ index_fr.php
@@ -4,7 +4,9 @@
  * Variables
  ***/
 
-$schedule_date_format = "d-m-Y";
+$schedule_date_format = 'd-m-Y';
+// $schedule_url = 'https://api3-eu.libcal.com/api_hours_grid.php?iid=3328&format=json&weeks=1&systemTime=1&lid=5832';
+$schedule_url = 'data_fr.json';
 
 
 /***
@@ -12,28 +14,31 @@ $schedule_date_format = "d-m-Y";
  ***/
 
 function get_final_schedule($schedule_array, $schedule_date = null) {
+    global $schedule_date_format;
     $schedule_return = key(array_count_values($schedule_array));
-    $schedule_return_bis = "";
+    $schedule_return_bis = '';
+    $schedule_first_open_day = 'Lundi';
+    $schedule_last_open_day = 'Vendredi';
     // If there is only one value in this array, return this value
     if(array_count_values($schedule_array)[$schedule_return] != 5 && !empty($schedule_date)) {
         // If there is a holiday in this week
-        if($schedule_array["Monday"] != $schedule_return) {
-            $schedule_return_bis .= "<li>Attention, fermé lundi " . date($schedule_date_format, strtotime($schedule_date)) . ".</li>";
+        if($schedule_array['Monday'] != $schedule_return) {
+            $schedule_return_bis .= '<li>Attention, fermé lundi ' . date($schedule_date_format, strtotime($schedule_date)) . '.</li>';
         }
-        if($schedule_array["Tuesday"] != $schedule_return) {
-            $schedule_return_bis .= "<li>Attention, fermé mardi " . date($schedule_date_format, strtotime($schedule_date . ' +1 day')) . ".</li>";
+        if($schedule_array['Tuesday'] != $schedule_return) {
+            $schedule_return_bis .= '<li>Attention, fermé mardi ' . date($schedule_date_format, strtotime($schedule_date . ' +1 day')) . '.</li>';
         }
-        if($schedule_array["Wednesday"] != $schedule_return) {
-            $schedule_return_bis .= "<li>Attention, fermé mercredi " . date($schedule_date_format, strtotime($schedule_date . ' +2 day')) . ".</li>";
+        if($schedule_array['Wednesday'] != $schedule_return) {
+            $schedule_return_bis .= '<li>Attention, fermé mercredi ' . date($schedule_date_format, strtotime($schedule_date . ' +2 day')) . '.</li>';
         }
-        if($schedule_array["Thursday"] != $schedule_return) {
-            $schedule_return_bis .= "<li>Attention, fermé jeudi " . date($schedule_date_format, strtotime($schedule_date . ' +3 day')) . ".</li>";
+        if($schedule_array['Thursday'] != $schedule_return) {
+            $schedule_return_bis .= '<li>Attention, fermé jeudi ' . date($schedule_date_format, strtotime($schedule_date . ' +3 day')) . '.</li>';
         }
-        if($schedule_array["Friday"] != $schedule_return) {
-            $schedule_return_bis .= "<li>Attention, fermé vendredi " . date($schedule_date_format, strtotime($schedule_date . ' +4 day')) . ".</li>";
+        if($schedule_array['Friday'] != $schedule_return) {
+            $schedule_return_bis .= '<li>Attention, fermé vendredi ' . date($schedule_date_format, strtotime($schedule_date . ' +4 day')) . '.</li>';
         }
     }
-    return Array($schedule_return, $schedule_return_bis);
+    return Array($schedule_return, $schedule_return_bis, $schedule_first_open_day, $schedule_last_open_day);
 }
 
 
@@ -42,7 +47,6 @@ function get_final_schedule($schedule_array, $schedule_date = null) {
  ***/
 
 // Collect data from Libcal API
-$schedule_url = "https://api3-eu.libcal.com/api_hours_grid.php?iid=3328&format=json&weeks=1&systemTime=1&lid=5832";
 $schedule_json = file_get_contents($schedule_url);
 $schedule_data = json_decode($schedule_json, TRUE);
 
@@ -56,7 +60,7 @@ $schedule_27rsg_week_open_array = Array(
     'Thursday' => $schedule_data['loc_5858']['weeks'][0]['Thursday']['times']['hours'][0]['from'],
     'Friday' => $schedule_data['loc_5858']['weeks'][0]['Friday']['times']['hours'][0]['from']
 );
-list($schedule_27rsg_week_open, $schedule_27rsg_week_message) = get_final_schedule($schedule_27rsg_week_open_array, $schedule_data['loc_5858']['weeks'][0]['Monday']['date']);
+list($schedule_27rsg_week_open, $schedule_27rsg_week_message, $schedule_first_open_day, $schedule_last_open_day) = get_final_schedule($schedule_27rsg_week_open_array, $schedule_data['loc_5858']['weeks'][0]['Monday']['date']);
 // 27 rue Saint Guillaume, Week, Closing hour
 $schedule_27rsg_week_close_array = Array(
     'Monday' => $schedule_data['loc_5858']['weeks'][0]['Monday']['times']['hours'][0]['to'],
@@ -94,52 +98,49 @@ $schedule_30rsg_saturday_open = $schedule_data['loc_5859']['weeks'][0]['Saturday
 $schedule_30rsg_saturday_close = $schedule_data['loc_5859']['weeks'][0]['Saturday']['times']['hours'][0]['to'];
 
 // Build the schedules bloc
-$schedule_block = "<li>Lundi - Vendredi:</li>";
+$schedule_block = "<li>$schedule_first_open_day - $schedule_last_open_day:</li>";
 $schedule_block .= "<li>$schedule_27rsg_week_open - $schedule_27rsg_week_close (27SG) | $schedule_30rsg_week_open - $schedule_30rsg_week_close (30SG)</li>";
 // If the library is closed on saturday
 if(empty($schedule_27rsg_saturday_open)) {
-    $schedule_27rsg_week_message .= "<li>Attention, fermé samedi " . date($schedule_date_format, strtotime($schedule_data['loc_5858']['weeks'][0]['Saturday']['date'])) . ".</li>";
+    $schedule_27rsg_week_message .= '<li>Attention, fermé samedi ' . date($schedule_date_format, strtotime($schedule_data['loc_5858']['weeks'][0]['Saturday']['date'])) . '.</li>';
 } else {
-    $schedule_block .= "<li>Samedi:</li>";
+    $schedule_block .= '<li>Samedi:</li>';
     $schedule_block .= "<li>$schedule_27rsg_saturday_open - $schedule_27rsg_saturday_close (27SG) | $schedule_30rsg_saturday_open - $schedule_30rsg_saturday_close (30SG)</li>";
 }
 if(!empty($schedule_27rsg_week_message)) {
     $schedule_block .= "$schedule_27rsg_week_message";
 }
 
-$schedule_html = "";
-$schedule_html .= "<div id=\"entre-etudiant\">";
-$schedule_html .= "<h2>Etudiants</h2>";
-$schedule_html .= "<ul>";
-$schedule_html .= "<li><a href=\"http://sciencespo.libcal.com/booking/salles-travail-groupe\" target=\"_blank\">Réserver une salle de travail</a></li>";
-$schedule_html .= "<li><a href=\"/bibliotheque/fr/etudier/photocopier-imprimer\">Imprimer, scanner, killprint</a></li>";
-$schedule_html .= "<li><a href=\"http://www.sciencespo.fr/bibliotheque/fr/rechercher/eressources\" target=\"_blank\">Lire la presse en ligne</a></li>";
-$schedule_html .= "<!--<li><a href=\"/bibliotheque/fr/a-votre-ecoute/formations\">Se former</a></li>";
-$schedule_html .= "<li><a href=\"/bibliotheque/fr/a-votre-ecoute/nous-contacter\">Rdv avec un-e bibliothécaire</a></li>-->";
-$schedule_html .= "<li><a href=\"/bibliotheque/fr/etudier/etudiants-campus-regions\">Campus en région</a></li>";
-$schedule_html .= "<li><a href=\"http://www.sciencespo.fr/bibliotheque/fr/a-votre-ecoute/enquetes/libqual/libqual2017\">Enquête Libqual+ 2017</a></li>";
-$schedule_html .= "</ul>";
-$schedule_html .= "</div>";
-$schedule_html .= "<div id=\"entre-enseignant\">";
-$schedule_html .= "<h2>Enseignants et Chercheurs</h2>";
-$schedule_html .= "<ul>";
-$schedule_html .= "<li><a href=\"http://www.sciencespo.fr/ecole-doctorale/fr/content/bibliotheque-de-lecole-doctorale\" target=\"_blank\" style=\"outline: 0px;\">Bibliothèque de recherche</a></li>";
-$schedule_html .= "<li><a href=\"/bibliotheque/fr/enseignants-chercheurs/gestion-donnees-recherche\">Gestion des données de la recherche</a></li>";
-$schedule_html .= "<li><a href=\"/bibliotheque/fr/enseignants-chercheurs/navette\">Navette chercheurs</a></li>";
-$schedule_html .= "<li><a href=\"https://docs.google.com/a/sciencespo.fr/forms/d/e/1FAIpQLSfVnVnYZIW8QpVm5p8TEjoQQccdLHDdThJa-jkj7Q_tqGqIwQ/viewform\" target=\"_blank\" style=\"outline: 0px;\">Numérisation à la demande</a></li>";
-$schedule_html .= "<li><a href=\"http://spire.sciences-po.fr/\" target=\"_blank\">Spire, l'archive ouverte</a></li>";
-$schedule_html .= "<li><a href=\"/bibliotheque/fr/rechercher/trucs-astuces\">Trucs et astuces</a></li>";
-$schedule_html .= "<!--<li><a href=\"/bibliotheque/fr/a-votre-ecoute/nous-contacter\">Rdv avec un-e bibliothécaire</a></li>-->";
-$schedule_html .= "</ul>";
-$schedule_html .= "</div>";
-$schedule_html .= "<div id=\"entre-venir\">";
-$schedule_html .= "<h2>Venir</h2>";
-$schedule_html .= "<ul>";
+$schedule_html = '';
+$schedule_html .= '<div id="entre-etudiant">';
+$schedule_html .= '<h2>Etudiants</h2>';
+$schedule_html .= '<ul>';
+$schedule_html .= '<li><a href="http://sciencespo.libcal.com/booking/salles-travail-groupe" target="_blank">Réserver une salle de travail</a></li>';
+$schedule_html .= '<li><a href="/bibliotheque/fr/etudier/photocopier-imprimer">Imprimer, scanner, killprint</a></li>';
+$schedule_html .= '<li><a href="http://www.sciencespo.fr/bibliotheque/fr/rechercher/eressources" target="_blank">Lire la presse en ligne</a></li>';
+$schedule_html .= '<li><a href="/bibliotheque/fr/etudier/etudiants-campus-regions">Campus en région</a></li>';
+$schedule_html .= '<li><a href="http://www.sciencespo.fr/bibliotheque/fr/a-votre-ecoute/enquetes/libqual/libqual2017">Enquête Libqual+ 2017</a></li>';
+$schedule_html .= '</ul>';
+$schedule_html .= '</div>';
+$schedule_html .= '<div id="entre-enseignant">';
+$schedule_html .= '<h2>Enseignants et Chercheurs</h2>';
+$schedule_html .= '<ul>';
+$schedule_html .= '<li><a href="http://www.sciencespo.fr/ecole-doctorale/fr/content/bibliotheque-de-lecole-doctorale" target="_blank">Bibliothèque de recherche</a></li>';
+$schedule_html .= '<li><a href="/bibliotheque/fr/enseignants-chercheurs/gestion-donnees-recherche">Gestion des données de la recherche</a></li>';
+$schedule_html .= '<li><a href="/bibliotheque/fr/enseignants-chercheurs/navette">Navette chercheurs</a></li>';
+$schedule_html .= '<li><a href="https://docs.google.com/a/sciencespo.fr/forms/d/e/1FAIpQLSfVnVnYZIW8QpVm5p8TEjoQQccdLHDdThJa-jkj7Q_tqGqIwQ/viewform" target="_blank">Numérisation à la demande</a></li>';
+$schedule_html .= '<li><a href="http://spire.sciences-po.fr/" target="_blank">Spire, l\'archive ouverte</a></li>';
+$schedule_html .= '<li><a href="/bibliotheque/fr/rechercher/trucs-astuces">Trucs et astuces</a></li>';
+$schedule_html .= '</ul>';
+$schedule_html .= '</div>';
+$schedule_html .= '<div id="entre-venir">';
+$schedule_html .= '<h2>Venir</h2>';
+$schedule_html .= '<ul>';
 $schedule_html .= $schedule_block;
-$schedule_html .= "<li><a href=\"http://www.sciencespo.fr/bibliotheque/fr/venir/horaires\" style=\"line-height: 16px;\">Tous les horaires</a></li>";
-$schedule_html .= "<li><a href=\"/bibliotheque/fr/venir/conditions-acces\" style=\"line-height: 16px;\">S'inscrire</a></li>";
-$schedule_html .= "</ul>";
-$schedule_html .= "</div>";
+$schedule_html .= '<li><a href="http://www.sciencespo.fr/bibliotheque/fr/venir/horaires">Tous les horaires</a></li>';
+$schedule_html .= '<li><a href="/bibliotheque/fr/venir/conditions-acces">S\'inscrire</a></li>';
+$schedule_html .= '</ul>';
+$schedule_html .= '</div>';
 
 // Display the whole block
 print $schedule_html;
